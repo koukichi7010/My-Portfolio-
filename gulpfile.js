1b2f31daee1c9d913ccdc809072e4011679e94b9
Koukichi Sakamoto <kessenkymalacas@gmail.com>
2024-03-05 20:23:29 +0800

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
+
 // Default Gulp Task
 
 exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
